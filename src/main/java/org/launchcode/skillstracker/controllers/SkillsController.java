f028a9db376c1fd5d4f238f68e5deeed8772ccdf
Andy Haag <andyhaag@gmail.com>
2023-09-22 09:22:50 -0500

    finish studio

---
+++ src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
@@ -0,0 +1,79 @@
+package org.launchcode.skillstracker.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class SkillsController {
+
+  //Handler that corresponds to localhost: 8080
+    @GetMapping("/")
+    public String displayHomePage() {
+        return "<html>" +
+                "<body>" +
+                "<h1>Skills Tracker</h1>" +
+                "<h2> We have a few skills we would like to learn.  Here is the list.</h2>" +
+                "<ol>" +
+                "<li>Java</li>" +
+                "<li>JavaScript</li>" +
+                "<li>Python</li>" +
+                "</ol>" +
+                "<p>Click <a href='/form'>here</a> to select your favorite.</p>" +
+                "</body>" +
+                "</html>";
+    }
+
+    @GetMapping("/form")
+    public String displaySkillsForm() {
+        return "<html>" +
+                "<body>" +
+                "<form action='/form' method='POST'>" +
+                "<label>Name</label>" +
+                "<input type='text' name='userName'/><br />" +
+                "<label>My favorite language:</label>" +
+                "<select name='fav1'>" +
+                "<option value='Java'>Java</option>" +
+                "<option value='JavaScript'>JavaScript</option>" +
+                "<option value='Python'>Python</option>" +
+                "</select><br />" +
+                "<label>My second favorite language:</label>" +
+                "<select name='fav2'>" +
+                "<option value='Java'>Java</option>" +
+                "<option value='JavaScript'>JavaScript</option>" +
+                "<option value='Python'>Python</option>" +
+                "</select><br />" +
+                "<label>My third favorite language:</label>" +
+                "<select name='fav3'>" +
+                "<option value='Java'>Java</option>" +
+                "<option value='JavaScript'>JavaScript</option>" +
+                "<option value='Python'>Python</option>" +
+                "</select><br />" +
+                "<input type='submit' value='Submit' />" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+    }
+
+    //Handler that corresponds to localhost:8080/form to process the form and display results
+
+    @PostMapping("/form")
+    public String processSkillsForm(@RequestParam String userName, String fav1, String fav2, String fav3) {
+        return "<html>" +
+                "<body>" +
+                "<h1>" + userName + "</h1>" +
+                "<h3>Favorite Languages</h3>" +
+                "<ol>" +
+                "<li>" + fav1 + "</li>" +
+                "<li>" + fav2 + "</li>" +
+                "<li>" + fav3 + "</li>" +
+                "</ol>" +
+                "</body>" +
+                "</html>";
+    }
+
+}
